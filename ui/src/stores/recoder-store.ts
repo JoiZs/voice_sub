b0a26607c1698fa279407a8b33e0294bc0eac16a
JoiZs <pyaesoneaung1126@hotmail.com>
2025-08-16 11:06:35 -0400

    feat(store): persist store with indexdb

--- ui/src/stores/recoder-store.ts
+++ ui/src/stores/recoder-store.ts
@@ -1,25 +1,31 @@
 import { createStore } from "zustand";
+import { get, set as dbset } from "idb-keyval";
 
 export type RecorderState = {
   inputDevice?: string;
 };
 
 export type RecoderActions = {
-  changeInput: () => void;
+  changeInput: (dev: string) => void;
   startRecording: () => void;
   clearRecording: () => void;
 };
 
 export type RecorderStore = RecorderState & RecoderActions;
 
-export const defaultInitState: RecorderState = { inputDevice: undefined };
+export const defaultInitState: RecorderState = {
+  inputDevice: (await get("rec_dev_id")) || undefined,
+};
 
 export const createRecorderStore = (
   initState: RecorderState = defaultInitState
 ) => {
   return createStore<RecorderStore>()((set, get) => ({
     ...initState,
-    changeInput: () => {},
+    changeInput: async (dev: string) => {
+      await dbset("rec_dev_id", dev);
+      set({ inputDevice: dev });
+    },
     startRecording: () => {},
     clearRecording: () => {},
   }));
